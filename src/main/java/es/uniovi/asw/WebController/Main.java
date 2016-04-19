003ce3bbcde7dea387b373511c39fa811639f96b
daplerio <dario95tineo@gmail.com>
2016-04-19 14:27:49 +0200

    Correcion de bug

--- src/main/java/es/uniovi/asw/WebController/Main.java
+++ src/main/java/es/uniovi/asw/WebController/Main.java
@@ -1,6 +1,5 @@
 package es.uniovi.asw.WebController;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,20 +11,21 @@ import java.util.Map;
 
 @Controller
 public class Main {
-
-  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
-    public static Map<String, Integer> resultados;
+	public static Map<String, Integer> resultados;
 
   @RequestMapping("/")
   public String landing(Model model) {
-    LOG.info("Landing page access");
-
+    
       // Datos falsos temporales
       resultados = new HashMap<String, Integer>();
       resultados.put("SI", 50);
       resultados.put("NO", 50);
 
-    model.addAttribute("resultados",prepararArray());
+      int[] aux = new int[2];
+      aux[0] = 70;
+      aux[1] = 30;
+      
+    model.addAttribute("resultados", aux);
     return "index";
   }
 
