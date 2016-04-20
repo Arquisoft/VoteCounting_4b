3f19bc550ddc33623024574c4738b4a4837dff8c
daplerio <dario95tineo@gmail.com>
2016-04-20 09:52:22 +0200

    Coreccion errores base de datos

--- src/test/java/es/uniovi/asw/DBManagementTest.java
+++ src/test/java/es/uniovi/asw/DBManagementTest.java
@@ -27,8 +27,6 @@ public class DBManagementTest {
 
 	@Before
 	public void before() {
-		repository.deleteAll();
-		colegioRepository.deleteAll();
 
 		colegioRepository.save(new PollingStation(new Long(50), "Asturias", "Principado de Asturias"));
 		colegioRepository.save(new PollingStation(new Long(51), "Madrid", "Comunidad de Madrid"));
@@ -55,13 +53,33 @@ public class DBManagementTest {
 
 	@Test
 	public void testFindAllVotes() {
-		System.out.println(repository.findAll().size());
-		assertTrue(repository.findAll().size() == 0);
+		colegioRepository.save(new PollingStation(new Long(50), "Asturias", "Principado de Asturias"));
+		colegioRepository.save(new PollingStation(new Long(51), "Madrid", "Comunidad de Madrid"));
+		colegioRepository.save(new PollingStation(new Long(52), "León", "Comunidad de Castillo y León"));
+		colegioRepository.save(new PollingStation(new Long(53), "Extremadura", "Comunidad de Extremadura"));
+
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Asturias").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Asturias").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Asturias").get(0), Candidate.NO));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Asturias").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Madrid").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Madrid").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("León").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("León").get(0), Candidate.NO));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("León").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("León").get(0), Candidate.NO));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Extremadura").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Extremadura").get(0), Candidate.NO));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Extremadura").get(0), Candidate.SI));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Extremadura").get(0), Candidate.NO));
+		repository.save(new Vote(colegioRepository.findByCircunscripcion("Extremadura").get(0), Candidate.SI));
+
+		assertTrue(repository.findAll().size() == 15);
 	}
 
 	@Test
 	public void testFindByCandidate() {
-		assertTrue(repository.findByCandidatura(Candidate.SI).size() == 0);
+		assertTrue(repository.findByCandidatura(Candidate.SI).size() == 10);
 	}
 
 }
